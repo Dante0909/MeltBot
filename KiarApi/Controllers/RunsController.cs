0f04e3d5d85e5a85139285d1d893c78ff0c6e72d
_Dante09 <mathglmtl@gmail.com>
2021-11-21 20:42:08 -0500

    now using kiara

--- KiarApi/Controllers/RunsController.cs
+++ KiarApi/Controllers/RunsController.cs
@@ -1,4 +1,4 @@
-﻿using PassionLib;
+﻿/*using PassionLib;
 using PassionLib.DAL;
 using PassionLib.Models;
 using Microsoft.AspNetCore.Http;
@@ -24,6 +24,51 @@ namespace KiarApi.Controllers
         public async Task<ActionResult<List<Run>>> GetRuns()
         {
             return await context.Runs.Take(10).ToListAsync();
+        }
+    }
+}
+*/
+using PassionLib;
+using PassionLib.DAL;
+using PassionLib.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace KiarApi.Controllers
+{
+    public class RunsController : BaseApiController
+    {
+        private readonly RunsContext context;
+
+        //public RunsController(IConfiguration configuration, RunsContext context) : this(context)
+        //{
+        //    this.context = context;
+        //}
+        public RunsController(RunsContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetRuns()
+        {
+            ////Run r = context.Runs.Take(1).First();
+            //User u = new User()
+            //{
+            //    Id = 1
+            //};
+
+            //Run r = new Run(new Quest(94042801, "【高難易度】護法少女スペシャルヒーローショー"), "link", new Servant(603800, "シャルロット・コルデー"), u);
+
+            //string str = JsonConvert.SerializeObject(r, Formatting.Indented);
+
+            ////Run s = JsonConvert.DeserializeObject<Run>(str);
+            ////Console.WriteLine(s.Quest.Id);
+
+            //return str;
+            return Ok(await context.Runs.Include(r => r.Quest).Include(r => r.Dps).Include(r => r.Submitter).ToListAsync());
         }
     }
 }
--- KiarApi/Program.cs
+++ KiarApi/Program.cs
@@ -1,12 +1,12 @@
-using PassionLib.DAL;
+/*using PassionLib.DAL;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add config shared across projects
-/*var sharedConfigPath = Path.GetFullPath(Path.Combine(@"../secrets/sharedconfig.json"));
-builder.Configuration.AddJsonFile(sharedConfigPath);*/
+//var sharedConfigPath = Path.GetFullPath(Path.Combine(@"../secrets/sharedconfig.json"));
+//builder.Configuration.AddJsonFile(sharedConfigPath);
 builder.Configuration.AddEnvironmentVariables();
 
 // Add services to the container.
@@ -67,6 +67,79 @@ if (app.Environment.IsDevelopment())
 }
 if (app.Environment.IsProduction())
 {
+    //app.UseHsts();
+    using (var scope = app.Services.CreateScope())
+    {
+        var context = scope.ServiceProvider.GetRequiredService<RunsContext>();
+        if (context.Database.GetPendingMigrations().Any())
+        {
+            context.Database.Migrate(); // breaks if there's more than one instance running
+        }
+    }
+}
+
+//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
+app.UseBlazorFrameworkFiles();
+app.MapRazorPages();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+app.Run();
+*/
+using PassionLib.DAL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.ResponseCompression;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddHttpClient();
+
+var connectionString = builder.Configuration.GetConnectionString("postgres");
+connectionString ??= @"host=localhost;port=5432;Database=aecrdb;User ID=postgres;Password=DefaultLUL;";
+builder.Services.AddDbContext<RunsContext>(options =>
+{
+    options.UseNpgsql(connectionString);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
+}
+//else
+//{
+//    app.UseExceptionHandler("/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
+
+var doDbRecreation = bool.Parse(Environment.GetEnvironmentVariable("DO_DB_RECREATION") ?? "false");
+if (doDbRecreation)
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var context = scope.ServiceProvider.GetRequiredService<RunsContext>();
+        context.Database.EnsureDeleted();
+        context.Database.Migrate();
+        RunDbInitializer.Initialize(context);
+    }
+}
+if (app.Environment.IsProduction())
+{
     /*app.UseHsts();*/
     using (var scope = app.Services.CreateScope())
     {
@@ -78,14 +151,17 @@ if (app.Environment.IsProduction())
     }
 }
 
-/*app.UseHttpsRedirection();*/
 app.UseHttpsRedirection();
+
 app.UseBlazorFrameworkFiles();
-app.MapRazorPages();
+app.UseStaticFiles();
+
 app.UseRouting();
 
 app.UseAuthorization();
 
+app.MapRazorPages();
 app.MapControllers();
 app.MapFallbackToFile("index.html");
+
 app.Run();
