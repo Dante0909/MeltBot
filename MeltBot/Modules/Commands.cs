9b4a03d22853ac4ca9775c2c4bbf0f7e0a2257f9
_Dante09 <mathglmtl@gmail.com>
2021-12-05 23:18:43 -0500

    more debug on exception

--- MeltBot/Modules/Commands.cs
+++ MeltBot/Modules/Commands.cs
@@ -218,7 +218,7 @@ namespace MeltBot.Modules
             catch (Exception ex)
             {
                 Console.WriteLine(ex);
-                await ctx.Channel.SendMessageAsync(ex.Message);
+                await ctx.Channel.SendMessageAsync(ex.ToString());
             }
         }
         [Command("website")]
